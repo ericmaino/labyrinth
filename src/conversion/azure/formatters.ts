e7831e7d4756e18a924c1f6958b853a227997463
Eric Maino <eric.maino@microsoft.com>
2021-03-08 09:58:36 -0800

    FEATURE: Implement Functional Multi-Pass walk
    - The first walk is focused on establishing an intermediate object
      as well as advertising and connecting relationships. No node may
      take a dependency on another node in the first graph. It may only
      use resource graph spec information for which it owns
    
    - The second pass of the walk is about materializing labyrinth nodes
      in this pass nodes may depend on other nodes and can query for
      those which are related.
    
    EXCEPTIONS
    - There is one exception at the moment in the first pass virtual ips
      and network interfaces need to be able to query the Virtual Machine
      Scale Set which owns them. A present there does not appear to be
      a good way around this.

---
+++ src/conversion/azure/formatters.ts
@@ -0,0 +1,25 @@
+import {hash} from '../../utilities/hash';
+import {AzureReference, AzureSubnet, SubnetKeys} from './types';
+
+export function normalizedSymbolKey(input: string): string {
+  const parts = input.split(/[/]/).reverse();
+  const head = parts.slice(0, 2).join('_');
+  const signature = hash(input).slice(0, 6);
+  return [head, 'ServiceTag', signature].join('_').split(/[*/-]/).join('_');
+}
+
+export function normalizedNodeKey(input: string): string {
+  return input.split('/').slice(6).reverse().join('/');
+}
+
+export function subnetKeys(input: AzureReference<AzureSubnet>): SubnetKeys {
+  // Our convention is to use the Azure id as the Labyrinth NodeSpec key.
+  const prefix = normalizedNodeKey(input.id);
+
+  // TODO: come up with safer naming scheme. Want to avoid collisions
+  // with other names.
+  const inbound = prefix + '/inbound';
+  const outbound = prefix + '/outbound';
+
+  return {prefix, inbound, outbound};
+}
