a6245b9e2cb0fef19c217f2d96140ede5dff2067
Eric Maino <ericmai@microsoft.com>
2021-03-30 10:53:20 -0700

    FIX: Elegantly handle unbound network interfaces

--- test/conversion/azure/convert_nic.test.ts
+++ test/conversion/azure/convert_nic.test.ts
@@ -1,7 +1,7 @@
 import {assert} from 'chai';
 import 'mocha';
 
-import {NodeSpec} from '../../../src';
+import {NodeSpec, SimpleRoutingRuleSpec} from '../../../src';
 
 import {convertNIC} from '../../../src/conversion/azure';
 
@@ -106,12 +106,20 @@ export default function test() {
       assert.deepEqual(observedNodes, expectedNodes);
     });
 
-    it('Guard check for missing VM', () => {
+    it('Mising VM should result in unbounded node', () => {
       const {services} = createGraphServicesMock();
 
-      assert.throws(() => {
-        convertNIC(services, nicWithoutVm, subnet1Id, vnet1Id);
-      }, 'NIC without VM are not supported');
+      convertNIC(services, nicWithoutVm, subnet1Id, vnet1Id);
+      const inboundNode = services.nodes.get(nic1InboundKey);
+      assert.deepEqual(inboundNode, {
+        key: nic1InboundKey,
+        name: `${nic1Id}/inbound`,
+        routes: [
+          {
+            destination: 'UnboundNetworkInterface',
+          },
+        ],
+      });
     });
   });
 }
