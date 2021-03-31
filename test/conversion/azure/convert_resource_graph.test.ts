c3d00f87db76751466ab3c046395d71f4132ef50
Eric Maino <ericmai@microsoft.com>
2021-03-31 09:37:18 -0700

    REFACTOR: Removing unused backbone inbound node

--- test/conversion/azure/convert_resource_graph.test.ts
+++ test/conversion/azure/convert_resource_graph.test.ts
@@ -22,10 +22,6 @@ export default function test() {
     it('validate default creation of internet and backbone', () => {
       const expectedNodes = [
         {
-          key: 'AzureBackbone/inbound',
-          routes: [],
-        },
-        {
           key: 'AzureBackbone/outbound',
           routes: [
             {
@@ -43,18 +39,6 @@ export default function test() {
       const emptyGraph: AnyAzureObject[] = [];
       const result = convert(emptyGraph);
       assert.deepEqual(result.graph.nodes, expectedNodes);
-    });
-
-    it.skip('backbone should not have inbound vnet routes', () => {
-      // Test currently skipped as convert has an adverse reaction which normalizes
-      // the ids which then breaks future tests. There are a few ways to fix this
-      // however not addressed until we can chat about it.
-      const graphWithVnet: AnyAzureObject[] = [vnet1, nsg1];
-      const result = convert(graphWithVnet);
-      const inboundNode = result.graph.nodes.find(
-        x => x.key === 'AzureBackbone/inbound'
-      );
-      assert.deepEqual(inboundNode?.routes, []);
     });
   });
 }
